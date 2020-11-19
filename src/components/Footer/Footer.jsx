0a996cac5c6bcad2981a302e834f99a80449a88b
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-19 17:09:33 -0500

    added basic sticky footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -2,9 +2,15 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
-            <h1>Footer</h1>
+        <footer className="footer mt-auto py-1 bg-light">
+        <div className="container-fluid">
+          <div className="row">
+            <span className="col-sm-12 text-center my-auto">
+              Copyright&copy; 2020 Aimee Corbin Esler
+            </span>
+          </div>
         </div>
+      </footer>
     );
 };
 
