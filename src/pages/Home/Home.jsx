32f3f75b877b98a9c9d357260e8b965cc68ca99e
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-19 19:38:21 -0500

    added basic form to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,16 +3,36 @@ import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 import "./Home.css";
 import Col from "react-bootstrap/Col";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
 
 const Home = () => {
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    console.log("SUBMIT");
+  };
+
   return (
-      <Container fluid className="flex-fill home-container vh-100 align-items-center">
-        <Row className="h-100">
-            <Col className="text-center my-auto">
-            <h1>HOME</h1>
-            </Col>
-        </Row>
-      </Container>
+    <Container
+      fluid
+      className="flex-fill home-container vh-100 align-items-center"
+    >
+      <Row className="h-100">
+        <Col sm={4} className="text-center m-auto">
+          <Form onSubmit={handleSubmit}>
+            <Form.Group controlId="formSearch">
+              <Form.Control
+                type="text"
+                placeholder="Address, City, Zip, Community or MLS Number"
+              />
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Submit
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
