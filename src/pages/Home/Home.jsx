39998dfea4af5c28656c2899ccdd6de547d73a23
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-19 19:19:57 -0500

    added background image and vertical centering to home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import "./Home.css";
+import Col from "react-bootstrap/Col";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>Home</h1>
-        </div>
-    );
+  return (
+      <Container fluid className="flex-fill home-container vh-100 align-items-center">
+        <Row className="h-100">
+            <Col className="text-center my-auto">
+            <h1>HOME</h1>
+            </Col>
+        </Row>
+      </Container>
+  );
 };
 
 export default Home;
