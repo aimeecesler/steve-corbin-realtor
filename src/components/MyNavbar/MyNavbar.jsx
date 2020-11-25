164d59e10d13d0edb1551e98ed1ded40a5670c07
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-11-25 12:45:43 -0500

    added logo to navbar

--- src/components/MyNavbar/MyNavbar.jsx
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
 import Nav from "react-bootstrap/Nav";
+import watersedgeLogo from "../../images/WEbutton.png";
+import "./MyNavbar.css";
 
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-    <Navbar.Brand href="/">Steve Corbin - Realtor&trade;</Navbar.Brand>
+    <Navbar.Brand href="/">
+      <img src={watersedgeLogo} alt="logo" className="navbar-logo"/>
+    </Navbar.Brand>
     <Navbar.Toggle aria-controls="basic-navbar-nav" />
     <Navbar.Collapse id="basic-navbar-nav">
       <Nav className="ml-auto">
--- src/pages/Contact/Contact.jsx
+++ src/pages/Contact/Contact.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import Row from "react-bootstrap/Row";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Card from "react-bootstrap/Card";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
-    </div>
+    <Container>
+      <Row className="mt-5">
+        <Col sm={8}></Col>
+        <Col sm={4}>
+          <Card>
+            <Card.Body>
+              <Card.Title>Contact Me</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Card.Text>
+                Email:{" "}
+                <Card.Link href="MAILTO:stevecorbinrealtor@gmail.com">
+                  SteveCorbinRealtor@gmail.com
+                </Card.Link>
+              </Card.Text>
+              <Card.Text>
+                Phone:{" "}
+                <Card.Link href="tel:2396337255">(239) 633-7255</Card.Link>
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
