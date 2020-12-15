9fb58708f3b42082c76eb6dfc64f87b9beadc46d
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-12-15 15:42:37 -0500

    added basic card for listings

--- src/pages/CurrentListings/CurrentListings.jsx
+++ src/pages/CurrentListings/CurrentListings.jsx
@@ -1,10 +1,37 @@
 import React from "react";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Card from "react-bootstrap/Card";
+import Container from "react-bootstrap/Container";
+import Button from "react-bootstrap/Button";
 
 const CurrentListings = () => {
   return (
-    <div>
-      <h1>Current Listings</h1>
-    </div>
+    <Container>
+      <Row>
+        <Col sm={2}></Col>
+        <Col sm={8}>
+          <Card className="p-3">
+            <Row>
+              <Col sm={3}>
+                <Card.Img
+                  alt="cover photo"
+                  src="https://via.placeholder.com/250x250"
+                />
+              </Col>
+              <Col sm={9}>
+                <Card.Body>
+                  <Card.Title>Home Address</Card.Title>
+                  <Card.Text>
+                    Home details
+                  </Card.Text>
+                </Card.Body>
+              </Col>
+            </Row>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
