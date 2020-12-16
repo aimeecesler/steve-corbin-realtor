e4a26d09fa80ca22f02a39deec5d7d2ace287322
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-12-16 16:04:58 -0500

    fixed router issues for deployment to gh-pages

--- src/components/MyNavbar/MyNavbar.jsx
+++ src/components/MyNavbar/MyNavbar.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Navbar from "react-bootstrap/Navbar";
+import { Link, NavLink } from "react-router-dom";
 import Nav from "react-bootstrap/Nav";
 import logoSC from "../../images/SteveCorbin.png";
 import "./MyNavbar.css";
@@ -7,17 +8,17 @@ import "./MyNavbar.css";
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="/">
+      <Link className="avbar-brand" to="/">
         <img src={logoSC} alt="logo" className="navbar-logo" />
-      </Navbar.Brand>
+      </Link>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link href="/">Home</Nav.Link>
-          <Nav.Link href="/search">Search</Nav.Link>
-          <Nav.Link href="/contact">Contact</Nav.Link>
-          <Nav.Link href="/about">About</Nav.Link>
-          <Nav.Link href="/current-listings">Current Listings</Nav.Link>
+          <NavLink className="nav-link" to="/">Home</NavLink>
+          <NavLink className="nav-link" to="/search">Search</NavLink>
+          <NavLink className="nav-link" to="/contact">Contact</NavLink>
+          <NavLink className="nav-link" to="/about">About</NavLink>
+          <NavLink className="nav-link" to="/current-listings">Current Listings</NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
