f2e63e7c5a1989ddfda242f45b495f6b733a58d6
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-12-21 13:12:28 -0500

    added controller imports to server.js

--- server.js
+++ server.js
@@ -7,14 +7,17 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+const AuthController = require("./controllers/authController");
+const PropertyController = require("./controllers/propertyController");
+
 app.use(express.static("client/build"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-  }
+  app.use(express.static("client/build"));
+}
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/properties", {
   useNewUrlParser: true,
@@ -39,9 +42,12 @@ app.get("/api/config", (req, res) => {
   });
 });
 
+app.use("/current-listings", PropertyController);
+app.use(AuthController);
+
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-  });
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, () => {
   console.log(`App is running on http://localhost:${PORT}`);
