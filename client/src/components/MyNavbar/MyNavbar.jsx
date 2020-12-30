60227695e56874e7f6bde0d7e672447a704afcac
ACE <67081507+aimeecesler@users.noreply.github.com>
2020-12-30 17:12:44 -0500

    updated formatting on navbar

--- client/src/components/MyNavbar/MyNavbar.jsx
+++ client/src/components/MyNavbar/MyNavbar.jsx
@@ -9,20 +9,38 @@ import "./MyNavbar.css";
 const MyNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
-      <Link className="navbar-brand" to="/">
-        <img src={logoSC} alt="logo" className="navbar-logo" />
-      </Link>
-      <a className="navbar-brand" href="https://watersedgere.com/" target="_blank" rel="noreferrer">
-        <img src={WEbutton} alt="logo" className="navbar-logo-we" />
-      </a>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
+      <span>
+        <Link className="navbar-brand" to="/">
+          <img src={logoSC} alt="logo" className="navbar-logo" />
+        </Link>
+        <a
+          className="navbar-brand"
+          href="https://watersedgere.com/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <img src={WEbutton} alt="logo" className="navbar-logo-we" />
+        </a>
+      </span>
+
+      <Navbar.Toggle aria-controls="basic-navbar-nav" className="hamburger"/>
+      <Navbar.Collapse id="basic-navbar-nav" className="text-right">
         <Nav className="ml-auto">
-          <NavLink className="nav-link" to="/">Home</NavLink>
-          <NavLink className="nav-link" to="/search">Search</NavLink>
-          <NavLink className="nav-link" to="/contact">Contact</NavLink>
-          <NavLink className="nav-link" to="/about">About</NavLink>
-          <NavLink className="nav-link" to="/current-listings">Current Listings</NavLink>
+          <NavLink className="nav-link" to="/">
+            Home
+          </NavLink>
+          <NavLink className="nav-link" to="/search">
+            Search
+          </NavLink>
+          <NavLink className="nav-link" to="/contact">
+            Contact
+          </NavLink>
+          <NavLink className="nav-link" to="/about">
+            About
+          </NavLink>
+          <NavLink className="nav-link" to="/current-listings">
+            Current Listings
+          </NavLink>
         </Nav>
       </Navbar.Collapse>
     </Navbar>
